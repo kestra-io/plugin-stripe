0e09e88e8c8acc2d36cfff89e868b715d28bfb66
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-05 18:02:48 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/stripe/payment/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Stripe Payment",
+    description = "This sub-group of plugins contains tasks to interact with Stripe Payments.",
+    categories = { PluginSubGroup.PluginCategory.TOOL }
+)
+package io.kestra.plugin.stripe.payment;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
